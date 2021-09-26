265234f70ef64d892decae58b3bff60748544602
Noni86kg <nonisc2@gmail.com>
2021-09-26 12:37:17 +0200

    zadatak 3

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const input = document.getElementById('email')
 const btn = document.querySelector('button');
 const message = document.getElementById('message');
+const correctMails = ["nonisc2@gmail.com",
+"nikola.pavlovic@quantoxtechnology.com",
+"big.boy@gmail.com",
+"cowboys.from.hell@gmail.com",
+];
+
 
 btn.addEventListener("click", function(e) {
 
+    for (let i=0; i<correctMails.length; i++) {
+        if (correctMails[i] == input.value.toLowerCase()) {
+            return message.textContent = "";
+        }
+    }
     if ('' == input.value) {
         return message.textContent = "Oops! Please add your email";
     } else {
