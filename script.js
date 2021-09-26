fb735dc104c920992c05c6257654e79d57b77324
Noni86kg <nonisc2@gmail.com>
2021-09-26 22:52:21 +0200

    zadatak 3

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 const input = document.getElementById('email')
 const btn = document.querySelector('button');
 const message = document.getElementById('message');
-const correctMails = ["nonisc2@gmail.com",
-"nikola.pavlovic@quantoxtechnology.com",
-"big.boy@gmail.com",
-"cowboys.from.hell@gmail.com",
-];
 
+function validateEmail(email) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
 
+  
 btn.addEventListener("click", function(e) {
+    e.preventDefault();
 
-    for (let i=0; i<correctMails.length; i++) {
-        if (correctMails[i] == input.value.toLowerCase()) {
-            return message.textContent = "";
-        }
-    }
     if ('' == input.value) {
         return message.textContent = "Oops! Please add your email";
-    } else {
-        return message.textContent = "Oops! Please check your email";
+    } else if (!validateEmail(input.value)) {
+        return message.textContent = "Oops! Please check your emai";
+    } else if (validateEmail(input.value)) {
+        return message.textContent = "";
     }
 })
